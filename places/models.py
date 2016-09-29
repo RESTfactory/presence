7d40425c8fc7cc8b9ed2de10c2a6b29b7f8c1ca2
mcueto <cueto@live.cl>
2016-09-29 18:56:37 -0300

    Added reverse handler
    Added reverse GeoPy for Place model Address

--- places/models.py
+++ places/models.py
@@ -1,9 +1,22 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from geopy.geocoders import Nominatim
 
 class Place(models.Model):
-    address = models.CharField(max_length=30)
+    address = models.CharField(max_length=30, blank=True)
     latitude = models.CharField(max_length=30, blank=True)
     longitude = models.CharField(max_length=30, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.address
+
+def place_handler(sender, instance, *args, **kwargs):
+    geolocator = Nominatim()
+    location = None
+
+    if(not instance.address):
+        if(instance.latitude and instance.longitude):
+            location = geolocator.reverse("%s, %s" % (instance.latitude, instance.longitude))
+            instance.address = location.address
+
+pre_save.connect(place_handler, sender=Place, dispatch_uid="place_handler")
