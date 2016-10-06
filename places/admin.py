6c0be4af165ff6af7e664d251f90a1f1e0345dfe
mcueto <cueto@live.cl>
2016-10-06 15:57:47 -0300

    Handling IntegrityError
    Handling Integrity error when creating a place with the same place_id(autogenerated with Geopy and nominatim)

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,24 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db import DatabaseError, IntegrityError
 from .models import Place
 
-admin.site.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    def add_view(self, request, form_url='', extra_context=None):
+        try:
+            return super(PlaceAdmin, self).add_view(request, form_url, extra_context)
+        except (IntegrityError, DatabaseError) as e:
+
+            request.method = 'GET'
+            messages.error(request, e)
+            return super(PlaceAdmin, self).add_view(request, form_url, extra_context)
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        try:
+            return super(PlaceAdmin, self).change_view(request, object_id, form_url, extra_context)
+        except (IntegrityError, DatabaseError) as e:
+
+            request.method = 'GET'
+            messages.error(request, e)
+            return super(PlaceAdmin, self).change_view(request, object_id, form_url, extra_context)
+
+admin.site.register(Place, PlaceAdmin)
