7025546755664e170bfb14aadc119c7fa1dbdc18
mcueto <cueto@live.cl>
2016-10-06 18:50:32 -0300

    Added handler for Exceptions in REST API

--- places/views.py
+++ places/views.py
@@ -1,7 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Place
 from .serializers import PlaceSerializer
 
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        except Exception as e:
+            return Response({"msg":str(e)}, status=status.HTTP_400_BAD_REQUEST)
