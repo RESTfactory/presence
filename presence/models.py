afaf647ccf0e33a48d50b68416442c0e129c05b1
mcueto <cueto@live.cl>
2016-11-03 12:06:27 -0300

    Created session model

--- presence/models.py
+++ presence/models.py
@@ -3,35 +3,44 @@ from django.conf import settings
 from django.contrib.gis.db import models
 from places.models import Place
 
-class App(models.Model):
-    name = models.CharField(max_length=30)
+class DateTimeRegisterMixin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class App(DateTimeRegisterMixin):
+    name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
 
-class Entity(models.Model):
+class Entity(DateTimeRegisterMixin):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=30, blank=True)
     app = models.ForeignKey(App)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
-class Checkin(models.Model):
+class Session(DateTimeRegisterMixin):
+    entity = models.ForeignKey(Entity)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+    def __str__(self):
+        return str(self.created_at)
+
+class Checkin(DateTimeRegisterMixin):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    created_at = models.DateTimeField(auto_now_add=True)
     place = models.ForeignKey(Place)
 
     def __str__(self):
         return str(self.created_at)
 
-class Checkout(models.Model):
+class Checkout(DateTimeRegisterMixin):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    created_at = models.DateTimeField(auto_now_add=True)
     place = models.ForeignKey(Place)
 
     def __str__(self):
